726c989821f6b64ae37c16157a613302da0dd83a
DiosGonzalo <gdiosfiestas@gmail.com>
2026-01-14 14:21:45 +0100

    querys

--- src/main/java/com/salesianostriana/_4/_1/Repositorios/CitaRepository.java
+++ src/main/java/com/salesianostriana/_4/_1/Repositorios/CitaRepository.java
@@ -1,9 +1,33 @@
 package com.salesianostriana._4._1.Repositorios;
 
+import com.salesianostriana._4._1.Dtos.CitaListDto;
 import com.salesianostriana._4._1.Models.Cita;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface CitaRepository extends JpaRepository<Cita, Long> {
+
+    @Query("""
+            SELECT c
+                FROM Cita
+                WHERE c.estado = :status
+            """)
+    @EntityGraph(attributePaths = {"paciente"})
+    List<Cita> buscarCitaPorEstado(@Param("status")String status);
+
+    @EntityGraph(attributePaths = {"paciente"})
+    List<Cita> findByPacienteId(Long id);
+
+
+    Page<Cita> findByFechaBetween(LocalDateTime inicio, LocalDateTime fin, Pageable pageable); [8, 10]
+
 }
